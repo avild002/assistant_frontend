10aa71e6d56442c8f95c210b7189c351668e580a
avildoso <arturo.vildosola@gmail.com>
2023-12-01 20:08:43 -0300

    test commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Assistant } from './assistant';
+import './styles.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Assistant/>
   </React.StrictMode>,
 )
