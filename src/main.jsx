b1848d14babbe6c289c373450bd2fcb659ac5bb1
avildoso <arturo.vildosola@gmail.com>
2023-12-21 11:53:59 -0300

    Added new UIs

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Assistant } from './assistant';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import { AssistantApp } from "./AssistantApp";
+import "./styles.css";
+import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Assistant/>
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <AssistantApp />
+    </BrowserRouter>
+  </React.StrictMode>
+);
